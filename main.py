62821319ab9c161f77a93203676555402ea12c98
Blur1ng <jonnykborisov@gmail.com>
2025-05-01 23:17:15 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from app.notification.telegram.telegram_main import start_telegram_app
 import asyncio
-from fastapi import FastAPI
-
-app = FastAPI()
 
 if __name__ == '__main__':
     print("----------------------------------------------------------------------------------------------------------------------")
